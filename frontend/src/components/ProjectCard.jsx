1f2314c449e7b75e1fecd2243e86dc9387e93ef5
joshuajeong1 <joshua.jeong2@gmail.com>
2025-06-21 16:22:16 -0700

    Changed font to Raleway for now, formatting fix for medium screens

--- frontend/src/components/ProjectCard.jsx
+++ frontend/src/components/ProjectCard.jsx
@@ -1,5 +1,4 @@
 import SkillCard from './SkillCard'
-import { LuSparkle } from "react-icons/lu";
 import { FaStarOfLife } from "react-icons/fa";
 
 function ProjectCard({title, description, bullets = [], skills = []}) {
@@ -8,8 +7,8 @@ function ProjectCard({title, description, bullets = [], skills = []}) {
         <div className="col-span-1 text-center xl:text-start">
             <div>
                 <h1 className="text-2xl font-bold text-emerald-500">{title}</h1>
-                <p className="mt-4 text-gray-400">{description}</p>
-                <ul className="mt-8 text-white items-center xl:items-start flex flex-col mx-auto xl:mx-0"> 
+                <p className="mt-4 text-gray-400 max-w-3xl">{description}</p>
+                <ul className="mt-8 text-white items-center xl:items-start flex flex-col mx-auto xl:mx-0 max-w-3xl"> 
                     {bullets.map((point, index) => (
                         <li key={index} className="flex items-start justify-start md:justify-center xl:justify-start gap-2">
                             <FaStarOfLife className="text-fuchsia-400 w-4 h-4 shrink-0 mt-1 animate-[spin_10s_linear_infinite]" />
@@ -17,7 +16,7 @@ function ProjectCard({title, description, bullets = [], skills = []}) {
                         </li>
                     ))}
                 </ul>
-                <div className="flex flex-wrap mt-8 gap-2 justify-center xl:justify-start">
+                <div className="flex flex-wrap mt-8 gap-2 justify-center xl:justify-start max-w-3xl">
                     {skills.map((skill, i) => (
                         <SkillCard key={i} icon={skill.icon} name={skill.name}/>
                     ))}
