609b1e78051d2e4204c52f3fcc79dc46f7c67d33
joshuajeong1 <joshua.jeong2@gmail.com>
2025-06-27 03:24:07 -0700

    Added Vercel analytics

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import Home from './pages/Home'
+import { Analytics } from '@vercel/analytics/next'
 
 function App() {
   const [count, setCount] = useState(0)
   return (
     <>
+      <Analytics />
       <div>
         <Home />
       </div>
